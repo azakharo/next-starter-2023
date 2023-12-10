2cad8e1ae16ccd25b7afe7d64f4aa32b4747f67c
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2023-12-10 19:07:01 +0300

    husky: run the checks for all the files on push

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,12 +3,11 @@ module.exports = {
   'src/**/*.(ts|tsx)': () => 'npx tsc --noEmit',
 
   // Lint then format TypeScript and JavaScript files
-  'src/**/*.(ts|tsx|js)': filenames => [
-    `npx eslint --fix ${filenames.join(' ')}`,
-    `npx prettier --write ${filenames.join(' ')}`,
+  'src/**/*.(ts|tsx|js)': () => [
+    'npx eslint --fix .',
   ],
 
   // Format MarkDown and JSON
-  'src/**/*.(md|json)': filenames =>
-    `npx prettier --write ${filenames.join(' ')}`,
+  'src/**/*.(md|json)': () =>
+    'npx prettier --write .',
 };
